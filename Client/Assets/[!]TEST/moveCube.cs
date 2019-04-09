4a730395f4618d89ac39b7f081659aa8f3f8fc36
Artem S <sartemw@gmail.com>
2019-04-09 18:48:33 +0700

    test

---
+++ Client/Assets/[!]TEST/moveCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().AddForce(Vector3.up*5);
+	}
+	
+	
+}
